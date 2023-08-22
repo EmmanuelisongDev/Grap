7b5a9dc87487217f0d4ade16e221994bdae067a1
emman <emm@Gmail.com>
2023-08-22 22:14:57 +0100

    new commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,36 @@
-import Head from 'next/head'
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "@components/Layout";
+import Container from "@components/Container";
+import Button from "@components/Button";
+import products from "@data/products";
+import styles from "@styles/Page.module.scss";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
-import Layout from '@components/Layout';
-import Container from '@components/Container';
-import Button from '@components/Button';
-
-import products from '@data/products';
-
-import styles from '@styles/Page.module.scss'
-
-export default function Home() {
+export default function Home({ home, products }) {
+  const { heroTitle, heroText, heroLink, heroBackground } = home;
   return (
     <Layout>
       <Head>
-        <title>Space Jelly Gear</title>
+        <title>Grap</title>
         <meta name="description" content="Get your Space Jelly gear!" />
       </Head>
 
       <Container>
-        <h1 className="sr-only">Space Jelly Gear</h1>
+        <h1 className="sr-only">Grap</h1>
 
         <div className={styles.hero}>
-          <Link href="#">
+          <Link href={heroLink}>
             <a>
               <div className={styles.heroContent}>
-                <h2>Prepare for liftoff.</h2>
-                <p>Apparel that&apos;s out of this world!</p>
+                <h2>{heroTitle}</h2>
+                <p>{heroText}</p>
               </div>
-              <img className={styles.heroImage} src="/images/space-jelly-gear-banner.jpg" alt="" />
+              <img
+                className={styles.heroImage}
+                src={heroBackground.url}
+                alt=""
+              />
             </a>
           </Link>
         </div>
@@ -35,32 +38,75 @@ export default function Home() {
         <h2 className={styles.heading}>Featured Gear</h2>
 
         <ul className={styles.products}>
-          {products.slice(0, 4).map(product => {
+          {products?.map((product) => {
             return (
-              <li key={product.id}>
-                <Link href="#">
+              <li key={product.slug}>
+                <Link href={`/products/${product.slug}`}>
                   <a>
                     <div className={styles.productImage}>
-                      <img width="500" height="500" src={product.image} alt="" />
+                      <img
+                        width="500"
+                        height="500"
+                        src={product.image.url}
+                        alt=""
+                      />
                     </div>
-                    <h3 className={styles.productTitle}>
-                      { product.name }
-                    </h3>
-                    <p className={styles.productPrice}>
-                      ${ product.price }
-                    </p>
+                    <h3 className={styles.productTitle}>{product.name}</h3>
+                    <p className={styles.productPrice}>${product.price}</p>
                   </a>
                 </Link>
                 <p>
-                  <Button>
-                    Add to Cart
-                  </Button>
+                  <Button>Add to Cart</Button>
                 </p>
               </li>
-            )
+            );
           })}
         </ul>
       </Container>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const client = new ApolloClient({
+    uri: "https://api-ca-central-1.hygraph.com/v2/cllihvusa17bv01uf623t4rwa/master",
+    cache: new InMemoryCache(),
+  });
+  const { data } = await client.query({
+    query: gql`
+      query PageHome {
+        page(where: { slug: "home" }) {
+          id
+          heroLink
+          heroText
+          heroTitle
+          name
+          slug
+          heroBackground {
+            height
+            width
+            url
+          }
+        }
+        products {
+          name
+          price
+          slug
+          image {
+            height
+            width
+            url
+          }
+        }
+      }
+    `,
+  });
+  const home = data.page;
+  const products = data.products;
+  return {
+    props: {
+      home,
+      products,
+    },
+  };
 }
